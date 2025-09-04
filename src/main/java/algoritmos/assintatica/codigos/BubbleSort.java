ea456cef0c5a5ee64c8a1ced3a7258989c55448e
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2025-09-03 21:53:37 -0300

    feat: Criação do método Bubble Sort

--- src/main/java/algoritmos/assintatica/codigos/BubbleSort.java
+++ src/main/java/algoritmos/assintatica/codigos/BubbleSort.java
@@ -1,5 +1,33 @@
 package algoritmos.assintatica.codigos;
 
 public class BubbleSort {
-    
+   public static void bubbleSort(int[] vetor) {
+      int auxiliar;
+      int i, j;
+      boolean swapped;
+      System.out.println("Vetor desordenado");
+      System.out.println("\n");
+      for (i = 0; i < vetor.length - 1; i++) {
+         System.out.println(" " + vetor[i]);
+      }
+      System.out.println(" ");
+      for (i = 0; i < vetor.length - 1; i++) {
+         System.out.println("Loop: " + (i + 1));
+         swapped = false;
+         for (j = 0; j < vetor.length - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+               auxiliar = vetor[j];
+               vetor[j] = vetor[j + 1];
+               vetor[j + 1] = auxiliar;
+               swapped = true;
+            }
+         }
+         if (!swapped) break;
+         System.out.println("Vetor ordenado");
+         System.out.println("\n");
+         for (i = 0; i < vetor.length; i++) {
+            System.out.println(" " + vetor[i]);
+         }
+      }
+   }
 }
