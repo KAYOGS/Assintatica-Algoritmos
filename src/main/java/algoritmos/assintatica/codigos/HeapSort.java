aad4275b625dfd898e2ce9df2aed3ddf7d880bd1
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2025-09-10 20:06:54 -0300

    feat: Implementação dos algoritmos HeapSort e InsertionSort.

--- src/main/java/algoritmos/assintatica/codigos/HeapSort.java
+++ src/main/java/algoritmos/assintatica/codigos/HeapSort.java
@@ -1,5 +1,63 @@
 package algoritmos.assintatica.codigos;
 
 public class HeapSort {
-    
+    public static void heapSort(int[] vetor) {
+        int n = vetor.length;
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) Math.floor(Math.random() * vetor.length);
+        }
+
+        System.out.println("Vetor desordenado:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(vetor, n, i);
+        }
+
+        System.out.println("\nVetor construído:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+
+        for (int j = n - 1; j > 0; j--) {
+            int aux = vetor[0];
+            vetor[0] = vetor[j];
+            vetor[j] = aux;
+
+            heapify(vetor, j, 0);
+        }
+
+        System.out.println("\nOrdenado:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void heapify(int[] vetor, int n, int i) {
+        int raiz = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && vetor[left] > vetor[raiz]) {
+            raiz = left;
+        }
+
+        if (right < n && vetor[right] > vetor[raiz]) {
+            raiz = right;
+        }
+
+        if (raiz != i) {
+            int aux = vetor[i];
+            vetor[i] = vetor[raiz];
+            vetor[raiz] = aux;
+
+            heapify(vetor, n, raiz);
+        }
+    }
 }
--- src/main/java/algoritmos/assintatica/codigos/InsertionSort.java
+++ src/main/java/algoritmos/assintatica/codigos/InsertionSort.java
@@ -1,5 +1,25 @@
 package algoritmos.assintatica.codigos;
 
 public class InsertionSort {
-    
+    public static void insertionSort(int[] vetor) {
+        int j, aux;
+        int tamanhoV = 0;
+
+        for (int i = 0; i < tamanhoV; i++) {
+            vetor[i] = (int) (Math.random() * tamanhoV);
+        }
+
+        for (int i = 1; i < tamanhoV; i++) {
+            aux = vetor[i];
+            j = i - 1;
+            while (j >= 0 && vetor[j] > aux) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j = 1] = aux;
+        }
+        for (int i = 0; i < tamanhoV; i++) {
+            System.out.println(vetor[1]);
+        }
+    }
 }
